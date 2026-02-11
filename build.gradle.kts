cf105f8cc6695b9c770ea4a499be974a55a93c9c
Wesley Kleiman <wesleykleiman@Wesleys-MacBook-Pro.local>
2026-02-11 17:51:15 -0500

    Updated documentation and UI testing file

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,10 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    // Source: https://mvnrepository.com/artifact/org.mockito/mockito-core
+    testImplementation("org.mockito:mockito-core:5.21.0")
+    // Source: https://mvnrepository.com/artifact/org.mockito/mockito-junit-jupiter
+    testImplementation("org.mockito:mockito-junit-jupiter:5.21.0")
 }
 
 tasks.test {
