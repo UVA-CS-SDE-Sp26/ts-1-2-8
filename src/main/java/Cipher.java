ac2ae2cc0b683b06dac348aadf71823ca14ca58a
vikram <vikrammukherjee8@gmail.com>
2026-02-12 11:46:45 -0500

    Readded Cipher and CipherKey

--- src/main/java/Cipher.java
+++ src/main/java/Cipher.java
@@ -1,4 +1,46 @@
-package PACKAGE_NAME;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 
 public class Cipher {
+
+
+    public CipherKey loadKey(String keyPath) throws FileNotFoundException {
+        File key = new File (keyPath);
+        Scanner scanner = new Scanner(key);
+
+
+        String plain = scanner.nextLine().trim();
+        String cipher = scanner.nextLine().trim();
+        scanner.close();
+
+
+        if (plain.length() != cipher.length()) {
+            throw new IllegalArgumentException("Lengths are different");
+        }
+
+
+        return new CipherKey(plain, cipher);
+    }
+    public String decipher(String input, CipherKey key) {
+        String plain = key.getPlain();
+        String cipher = key.getCipher();
+
+
+        StringBuilder answer = new StringBuilder();
+
+
+        for (char character : input.toCharArray()) {
+            int position = cipher.indexOf(character);
+            if (position >= 0) {
+                answer.append(plain.charAt(position));
+            } else {
+                answer.append(character);
+            }
+        }
+
+
+        return answer.toString();
+    }
 }
--- src/main/java/CipherKey.java
+++ src/main/java/CipherKey.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class CipherKey {
+    private final String plain;
+    private final String cipher;
+
+
+    public CipherKey(String plain, String cipher) {
+        this.plain = plain;
+        this.cipher = cipher;
+    }
+
+
+    public String getPlain() {
+        return plain;
+    }
+
+
+    public String getCipher() {
+        return cipher;
+    }
 }
