650b7a59f2ddf815999af3c7d49cc8732053f605
Noah Crozier <croznoah@gmail.com>
2026-02-12 15:03:47 -0500

    remove unused import

--- src/main/java/FileHandler.java
+++ src/main/java/FileHandler.java
@@ -1,7 +1,6 @@
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
